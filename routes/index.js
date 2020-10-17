0b4da659ca26b68a0e613bc0c33e41163dd5d4c1
SeungMin Kim <k0102575@gmail.com>
2020-10-17 19:32:16 +0900

    email

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const router = express.Router();
 
+const nodemailer = require('nodemailer');
+const handlebars = require('handlebars');
+const fs = require('fs');
+require('dotenv').config();
+
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.json({ result: true });
+  res.render('/');
+});
+
+router.post('/', (req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.FROM_URL,
+      pass: process.env.FROM_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.FROM_URL,
+    to: process.env.TO_URL,
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!',
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.json({ error: error });
+    } else {
+      res.json({ result: true });
+    }
+  });
 });
 
 module.exports = router;
